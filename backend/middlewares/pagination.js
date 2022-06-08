34519735aa59301828cf49c968cfb1ec9bfe04df
lucalves3 <lucalves.rodrigues@gmail.com>
2022-06-07 22:09:48 -0300

    adiciona documentação da função de paginação

--- backend/middlewares/pagination.js
+++ backend/middlewares/pagination.js
@@ -1,9 +1,26 @@
+/**
+ * Essa função irá retornar um modelo padrão de paginação
+ * 
+ * @param   {req} req   Esse parametro irá receber o requirement
+ * @param   {max} max   Esse parâmetro (type: number) irá receber o tamanho do banco de dados da requisição
+ * @param   {div} div   Esse parâmetro (type: number) irá definir quantos dados queremos ter por página
+ * 
+ * Essa função retorna os seguintes dados abaixo:
+ * @example
+ * "nextPage": 1,
+    "maxPages": 1,
+    "page": 0,
+ */
+
 const Pagination = (req, max, div) => {
+  // busca em qual page a requisição foi feita
   const page = parseInt(req.query.page);
-  // const limit = parseInt(req.query.limit);
+
+  // determina um limite de páginas pegando a quantidade máxima de objetos e a permitida por página, exemplo 30(objetos)/15(maximo por página) = 2 paginas
   const limit = Math.floor(max / div);
+
+  // determina como máximo de paginas -1 para padronizar a pagina 0
   const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
   const pagination = {};
   if (limit < max) {
     if (page === limit || page > limit) {
@@ -11,14 +28,12 @@ const Pagination = (req, max, div) => {
         nextPage: null,
         maxPages: Math.floor(max / div),
         page: page,
-
       };
     } else {
       pagination.nextPage = {
         nextPage: page + 1,
         maxPages: Math.floor(max / div),
         page: page,
-
       };
     }
   }
--- backend/routes.js
+++ backend/routes.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { getAllUsers, CreateUser, DeleteUser, UpdateUser } = require('./controllers/userController');
+const {
+  getAllUsers,
+  CreateUser,
+  DeleteUser,
+  UpdateUser,
+} = require('./controllers/userController');
 
 const Routes = express.Router();
 
@@ -9,6 +14,5 @@ Routes.get('/users', getAllUsers);
 Routes.put('/users/:id', UpdateUser);
 Routes.post('/users', CreateUser);
 Routes.delete('/users/:id', DeleteUser);
-
 
 module.exports = Routes;
