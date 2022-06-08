09b7f666ceeaaa3f93f269cd42361be5c2b58c88
lucalves3 <lucalves.rodrigues@gmail.com>
2022-06-07 22:01:21 -0300

    Cria documentação para código de ObjectInterface

--- backend/middlewares/objectInterface.js
+++ backend/middlewares/objectInterface.js
@@ -1,3 +1,21 @@
+/**
+ * Essa função irá retornar um modelo padrão de objeto
+ * com finalidade de padronizar os retornos da api ao fazer uma requisição
+ * 
+ * 
+ * @param   {pagination} pagination   Esse parametro irá receber um objeto com dois estados, previousPage e nextPage deverá ser o retorno da função "Pagination"
+ * @param   {data} data   Esse parâmetro irá receber o objeto data vindo do banco de dados
+ * @param   {uniq} uniq   Esse parâmetro booleano irá definir se o retorno é para vários objetos (models) ou apenas um objeto (model)
+ * 
+ * Essa função retorna o seguinte objeto abaixo:
+ * @example
+ * "nextPage": 1,
+    "maxPages": 1,
+    "page": 0,
+    "models": {
+        "data": []
+      }
+ */
 const ObjectInterface = (pagination, data, uniq) => {
   const { previousPage, nextPage } = pagination;
   let modelsReturn = {
