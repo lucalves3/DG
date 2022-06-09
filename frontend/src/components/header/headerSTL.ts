4be6211cffdf8541f09e708abfef38ca29fc6553
lucalves3 <lucalves.rodrigues@gmail.com>
2022-06-08 22:51:19 -0300

    Header finalizado

---
+++ frontend/src/components/header/headerSTL.ts
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+
+export const HeaderSTL = styled.header`
+  background-color: #34364f;
+  width: 100vw;
+  height: 15vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  .title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+  h1 {
+    color: white;
+    font-family: 'Poppins';
+    margin-top: -20px;
+  }
+
+  .bottomBar {
+    background-color: #b8fc1c;
+    border-radius: 10px;
+    margin-top: -30px;
+    width: 100%;
+    height: 10px;
+    animation: shake 5s ease infinite both;
+    transform-origin: 50% 50%;
+    transition: 1s;
+  }
+
+  .bottomBar:hover {
+    background-color: red;
+    transition: 1s;
+  }
+
+  .profileImg {
+    width: 50px;
+    height: 50px;
+    border-radius: 100%;
+  }
+
+  section {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 10%;
+  }
+
+  a {
+    color: white;
+    font-family: 'Poppins';
+  }
+
+  .PipeBar {
+  height: 50px;
+  background-color: #b8fc1c;
+  border: 1px solid #b8fc1c;
+  margin: 0 10px 0 10px
+  }
+
+  @keyframes shake {
+   0% { transform:translate(0,0) }
+  1.78571% { transform:translate(5px,0) }
+  3.57143% { transform:translate(0,0) }
+  5.35714% { transform:translate(5px,0) }
+  7.14286% { transform:translate(0,0) }
+  8.92857% { transform:translate(5px,0) }
+  10.71429% { transform:translate(0,0) }
+  100% { transform:translate(0,0) }
+}
+`;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ContextProvider } from './context/context';
+import { Home } from './pages/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <ContextProvider>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </ContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
