e1f07bd67ebbc2dbb605527e615551a24453270c
lucalves3 <lucalves.rodrigues@gmail.com>
2022-06-09 19:04:11 -0300

    Reajuste no erro 500 e adição de formik

--- frontend/src/components/main/main.tsx
+++ frontend/src/components/main/main.tsx
@@ -2,6 +2,10 @@ import { UsersTable } from '../usersTable/usersTable';
 import { MainSTL } from './mainSTL';
 import { useFetch } from '../../services/useFetch';
 import { useState } from 'react';
+import { Field, Form, Formik, ErrorMessage } from 'formik';
+import api from '../../services/api';
+import { mutate } from 'swr';
+import Swal from 'sweetalert2';
 
 export function Main(): any {
   const { data } = useFetch(`users?page=0`);
@@ -10,18 +14,85 @@ export function Main(): any {
   return (
     <MainSTL>
       <button onClick={() => setGetFormik(true)}>Adicionar</button>
+      {getFormik ? (
+        <section className="addUser">
+          <button onClick={() => setGetFormik(false)}>X</button>
+          <Formik
+            // validationSchema={Schema}
+            initialValues={{ name: '', birthDate: '' }}
+            onSubmit={async (values) => {
+              try {
+                await api.post('/users', values);
+                Swal.fire({
+                  title: 'Usuário cadastrado com sucesso!',
+                  text: 'Observe o mutate da rota',
+                  timer: 4000,
+                  closeButtonAriaLabel: 'Ok'
+                }).then(() => mutate('users?page=0'));
+              } catch (error) {}
+              ;
+            }}
+            enableReinitialize
+          >
+            {({
+              values,
+              handleChange,
+              handleSubmit,
+              setFieldValue,
+              handleBlur,
+            }) => (
+              <Form>
+                <div className="modal">
+                  <label className="cards">
+                    <span className="span-label-formik-contri">Nome</span>
+                    <Field
+                      className="input-formik-contri"
+                      onBlur={handleChange('name')}
+                      name="name"
+                      type="text"
+                    />
+                    <ErrorMessage
+                      name="name"
+                      component="span"
+                      className="errorMessages"
+                    />
+                  </label>
+                </div>
+                <div className="modal">
+                  <label className="cards">
+                    <span className="span-label-formik-contri">
+                      Data de Nascimento
+                    </span>
+                    <Field
+                      className="input-formik-contri"
+                      name="birthDate"
+                      onBlur={handleChange('birthDate')}
+                      type="text"
+                    />
+                    <ErrorMessage
+                      name="birthDate"
+                      component="span"
+                      className="errorMessages"
+                    />
+                  </label>
+                </div>
+                <button type="submit" className="modal-detail">
+                  ADICIONAR
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </section>
+      ) : (
+        <></>
+      )}
       <div className="tagName">
-      <h2>Excluir</h2>
+        <h2>Excluir</h2>
         <h2>Nome</h2>
         <h2>Data de Aniversário</h2>
         <h2>Idade</h2>
       </div>
-      {getFormik ? (
-          <section className='addUser'>
-            <button onClick={() => setGetFormik(false)}>X</button>
-            aqui tem um modal
-          </section>) : (<></>)}
-      {data && 
+      {data &&
         data?.models?.data.map((e, index) => (
           <UsersTable
             id={e?.id}
